e4dc4fa83d407e985f7b57a5c22d83bf579fbb40
Arshdeep <arshdeep317@gmail.com>
2019-11-17 13:33:30 +0530

    1st version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer.jsx';
+import sample from './PathfindingVisualizer/sample';
+import 'bootstrap/dist/css/bootstrap.min.css';
+/*import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Nav from 'react-bootstrap/Nav'
+*/
 
 function App() {
   return (
+  	//<div>
+  	/*<div className="App">
+  		<modall> </modall>
+  	</div>*/
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PathfindingVisualizer> </PathfindingVisualizer>
     </div>
+    //</div>
   );
 }
 
